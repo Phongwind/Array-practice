d506d04e1b036ea6c1a1cdbab09a4be31eda4987
Phongwind <phongdesign0201@gmail.com>
2020-03-10 14:51:16 +0700

    add more question

--- script.js
+++ script.js
@@ -15,19 +15,9 @@ const inventors = [
 
 const numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
 
-let inventorsNameStartWithA = [];
-let inventorsContainN = [];
-let inventorsTwiceSame = [];
-let numberOdd = [];
-let number2Digits = [];
-let inventorsFirstName = [];
-let toUpperCaseName = [];
-let initialsName = [];
-let multiplied100 = [];
-let pow3 = [];
-
 
 //inventors name start with "a"
+let inventorsNameStartWithA = [];
 inventorsNameStartWithA = inventors.filter((name) => {
     if (name.charAt(0) === "A") {
         return inventors;
@@ -39,6 +29,7 @@ console.log('The inventors have first letter of name is "a" are ', inventorsName
 
 
 //inventors name contain "n"
+let inventorsContainN = [];
 inventorsContainN = inventors.filter((name) => {
     if (name.includes("n"))
         return inventors;
@@ -47,6 +38,7 @@ inventorsContainN = inventors.filter((name) => {
 console.log('The inventors have a name which cotain "n" are ', inventorsContainN)
 
 //twice
+let inventorsTwiceSame = [];
 inventorsTwiceSame = inventors.filter((name) => {
     let splitName = name.split(' ');
     let isDuplicate = false;
@@ -63,11 +55,13 @@ inventorsTwiceSame = inventors.filter((name) => {
 console.log('The inventors have a name which cotain "n" are ', inventerNameTwice)
 
 //Print out an array of the numbers which are odd.
+let numberOdd = [];
 numberOdd = numbers.filter((num) => {
     return num % 2 === 1;
 });
 
 //two digits
+let number2Digits = [];
 number2Digits = numbers.filter((number) => {
     if (number > 9 && number < 100) {
         return numbers
@@ -80,6 +74,7 @@ console.log('The number with two digits ', number2Digits);
 isPrime = numbers.filter((n) => { const regex = /^(11+?)\1+?$/; return !('1'.repeat(n).match(regex)) })
 
 //print out the first name of each inventor
+let inventorsFirstName = [];
 inventorsFirstName = inventors.map((name) => {
     let nameSplit = name.split(' ', 1);
     return nameSplit;
@@ -97,6 +92,7 @@ console.log("Print out the length of every inventor's full name.", lengthInvento
 
 
 //Print out all the inventors' names in uppercase.
+let toUpperCaseName = [];
 toUpperCaseName = inventors.map((name)=> {
     for (i=0 ; i < name.length; i++) {
       return name.toUpperCase();
@@ -117,10 +113,30 @@ inventors.map ((name) => {
   })
 
 //print multiple 100
+let multiplied100 = [];
 console.log("Print out an array of every number multiplied by 100", multiplied100 = numbers.map(num=>num*100));
 
 //Sort all the inventors in alphabetical order, A-Z.
-console.log("Sort all the inventors in alphabetical order, A-Z",sortAZ=inventors.sort())
+sortAZ=inventors.sort();
+console.log("Sort all the inventors in alphabetical order, A-Z",sortAZ)
+
+//Sort all the inventors in reverse alphabetical order, Z-A. Do not use the reverse method.
+sortZA= inventors.sort((a,b)=>{
+    if (a>b){return -1}; 
+    if (a<b){return 1}
+    return 0;
+})
+
+console.log("Sort all the inventors in reverse alphabetical order, Z-A. Do not use the reverse method", sortZA);
+
+//sort all the inventors
+inventors.sort((a,b) => {
+    // let lengthA = a.length;
+    // let lengthB = b.length;
+    // return lengthB -lengthA
+  
+    return a.length < b.length? 1:-1
+})
 
 
 
